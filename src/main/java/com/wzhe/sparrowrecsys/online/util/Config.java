1db0184154cb4bf76d9e914b7f292b403ec395eb
Wang Zhe <wzhe06@163.com>
2020-10-13 00:58:28 -0700

    change default db config

--- src/main/java/com/wzhe/sparrowrecsys/online/util/Config.java
+++ src/main/java/com/wzhe/sparrowrecsys/online/util/Config.java
@@ -4,5 +4,5 @@ public class Config {
     public static final String DATA_SOURCE_REDIS = "redis";
     public static final String DATA_SOURCE_FILE = "file";
 
-    public static String EMB_DATA_SOURCE = Config.DATA_SOURCE_REDIS;
+    public static String EMB_DATA_SOURCE = Config.DATA_SOURCE_FILE;
 }
