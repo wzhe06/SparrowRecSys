e9b9eb5b2bae473f08346f0fb38c1024231aa5f2
Wang Zhe <wzhe06@163.com>
2020-11-15 00:27:44 -0800

    refactor deepfm model

--- TFRecModel/src/com/wzhe/sparrowrecsys/offline/tensorflow/DeepFM.py
+++ TFRecModel/src/com/wzhe/sparrowrecsys/offline/tensorflow/DeepFM.py
@@ -1,15 +1,17 @@
 import tensorflow as tf
 
+# Training samples path, change to your local path
 TRAIN_DATA_URL = "file:///Users/zhewang/Workspace/SparrowRecSys/src/main/resources/webroot/sampledata/modelSamples.csv"
 samples_file_path = tf.keras.utils.get_file("modelSamples.csv", TRAIN_DATA_URL)
 
 
+# load sample as tf dataset
 def get_dataset(file_path):
     dataset = tf.data.experimental.make_csv_dataset(
         file_path,
         batch_size=12,
         label_name='label',
-        na_value="",
+        na_value="0",
         num_epochs=1,
         ignore_errors=True)
     return dataset
@@ -17,44 +19,13 @@ def get_dataset(file_path):
 
 # sample dataset size 110830/12(batch_size) = 9235
 raw_samples_data = get_dataset(samples_file_path)
-
 print(raw_samples_data)
 
+# split as test dataset and training dataset
 test_dataset = raw_samples_data.take(1000)
 train_dataset = raw_samples_data.skip(1000)
 
-genre_vocab = ['Film-Noir', 'Action', 'Adventure', 'Horror', 'Romance', 'War', 'Comedy', 'Western', 'Documentary',
-               'Sci-Fi', 'Drama', 'Thriller',
-               'Crime', 'Fantasy', 'Animation', 'IMAX', 'Mystery', 'Children', 'Musical']
-
-GENRE_FEATURES = {
-    'userGenre1': genre_vocab,
-    'userGenre2': genre_vocab,
-    'userGenre3': genre_vocab,
-    'userGenre4': genre_vocab,
-    'userGenre5': genre_vocab,
-    'movieGenre1': genre_vocab,
-    'movieGenre2': genre_vocab,
-    'movieGenre3': genre_vocab
-}
-
-categorical_columns = []
-for feature, vocab in GENRE_FEATURES.items():
-    cat_col = tf.feature_column.categorical_column_with_vocabulary_list(
-        key=feature, vocabulary_list=vocab)
-    emb_col = tf.feature_column.embedding_column(cat_col, 10)
-    categorical_columns.append(emb_col)
-
-numerical_columns = [tf.feature_column.numeric_column('releaseYear'),
-                     tf.feature_column.numeric_column('movieRatingCount'),
-                     tf.feature_column.numeric_column('movieAvgRating'),
-                     tf.feature_column.numeric_column('movieRatingStddev'),
-                     tf.feature_column.numeric_column('userRatingCount'),
-                     tf.feature_column.numeric_column('userAvgRating'),
-                     tf.feature_column.numeric_column('userRatingStddev')]
-
-preprocessing_layer = tf.keras.layers.DenseFeatures(numerical_columns + categorical_columns)
-
+# define input for keras model
 inputs = {
     'movieAvgRating': tf.keras.layers.Input(name='movieAvgRating', shape=(), dtype='float32'),
     'movieRatingStddev': tf.keras.layers.Input(name='movieRatingStddev', shape=(), dtype='float32'),
@@ -66,15 +37,38 @@ inputs = {
 
     'movieId': tf.keras.layers.Input(name='movieId', shape=(), dtype='int32'),
     'userId': tf.keras.layers.Input(name='userId', shape=(), dtype='int32'),
+    'userRatedMovie1': tf.keras.layers.Input(name='userRatedMovie1', shape=(), dtype='int32'),
+
     'userGenre1': tf.keras.layers.Input(name='userGenre1', shape=(), dtype='string'),
-    'movieGenre1': tf.keras.layers.Input(name='movieGenre1', shape=(), dtype='string')
-    # 'userRatedMovie1': tf.keras.layers.Input(name='userRatedMovie1', shape=(), dtype='int32')
+    'userGenre2': tf.keras.layers.Input(name='userGenre2', shape=(), dtype='string'),
+    'userGenre3': tf.keras.layers.Input(name='userGenre3', shape=(), dtype='string'),
+    'userGenre4': tf.keras.layers.Input(name='userGenre4', shape=(), dtype='string'),
+    'userGenre5': tf.keras.layers.Input(name='userGenre5', shape=(), dtype='string'),
+    'movieGenre1': tf.keras.layers.Input(name='movieGenre1', shape=(), dtype='string'),
+    'movieGenre2': tf.keras.layers.Input(name='movieGenre2', shape=(), dtype='string'),
+    'movieGenre3': tf.keras.layers.Input(name='movieGenre3', shape=(), dtype='string'),
 }
 
+# movie id embedding feature
 movie_col = tf.feature_column.categorical_column_with_identity(key='movieId', num_buckets=1001)
-user_col = tf.feature_column.categorical_column_with_identity(key='userId', num_buckets=30001)
 movie_emb_col = tf.feature_column.embedding_column(movie_col, 10)
+
+# user id embedding feature
+user_col = tf.feature_column.categorical_column_with_identity(key='userId', num_buckets=30001)
 user_emb_col = tf.feature_column.embedding_column(user_col, 10)
+
+# genre features vocabulary
+genre_vocab = ['Film-Noir', 'Action', 'Adventure', 'Horror', 'Romance', 'War', 'Comedy', 'Western', 'Documentary',
+               'Sci-Fi', 'Drama', 'Thriller',
+               'Crime', 'Fantasy', 'Animation', 'IMAX', 'Mystery', 'Children', 'Musical']
+# user genre embedding feature
+user_genre_col = tf.feature_column.categorical_column_with_vocabulary_list(key="userGenre1",
+                                                                           vocabulary_list=genre_vocab)
+user_genre_emb_col = tf.feature_column.embedding_column(user_genre_col, 10)
+# item genre embedding feature
+item_genre_col = tf.feature_column.categorical_column_with_vocabulary_list(key="movieGenre1",
+                                                                           vocabulary_list=genre_vocab)
+item_genre_emb_col = tf.feature_column.embedding_column(item_genre_col, 10)
 
 deep_feature_columns = [tf.feature_column.numeric_column('releaseYear'),
                         tf.feature_column.numeric_column('movieRatingCount'),
@@ -86,48 +80,44 @@ deep_feature_columns = [tf.feature_column.numeric_column('releaseYear'),
                         movie_emb_col,
                         user_emb_col]
 
-user_genre_col = tf.feature_column.categorical_column_with_vocabulary_list(key="userGenre1",
-                                                                           vocabulary_list=genre_vocab)
-item_genre_col = tf.feature_column.categorical_column_with_vocabulary_list(key="movieGenre1",
-                                                                           vocabulary_list=genre_vocab)
-user_genre_emb_col = tf.feature_column.embedding_column(user_genre_col, 10)
-item_genre_emb_col = tf.feature_column.embedding_column(item_genre_col, 10)
-
 item_emb_layer = tf.keras.layers.DenseFeatures([movie_emb_col])(inputs)
 user_emb_layer = tf.keras.layers.DenseFeatures([user_emb_col])(inputs)
 item_genre_emb_layer = tf.keras.layers.DenseFeatures([item_genre_emb_col])(inputs)
 user_genre_emb_layer = tf.keras.layers.DenseFeatures([user_genre_emb_col])(inputs)
 
+# FM part, cross different categorical feature embeddings
 product_layer_item_user = tf.keras.layers.Dot(axes=1)([item_emb_layer, user_emb_layer])
 product_layer_item_genre_user_genre = tf.keras.layers.Dot(axes=1)([item_genre_emb_layer, user_genre_emb_layer])
 product_layer_item_genre_user = tf.keras.layers.Dot(axes=1)([item_genre_emb_layer, user_emb_layer])
 product_layer_user_genre_item = tf.keras.layers.Dot(axes=1)([item_emb_layer, user_genre_emb_layer])
 
-#deep part
+# deep part, MLP to generalize all input features
 deep = tf.keras.layers.DenseFeatures(deep_feature_columns)(inputs)
 deep = tf.keras.layers.Dense(64, activation='relu')(deep)
 deep = tf.keras.layers.Dense(64, activation='relu')(deep)
 
+# concatenate fm part and deep part
 concat_layer = tf.keras.layers.concatenate([product_layer_item_user, product_layer_item_genre_user_genre,
                                             product_layer_item_genre_user, product_layer_user_genre_item, deep], axis=1)
 output_layer = tf.keras.layers.Dense(1, activation='sigmoid')(concat_layer)
 
-deep_fm_model = tf.keras.Model(inputs, output_layer)
-deep_fm_model.compile(optimizer='adam',
-                      loss='binary_crossentropy',
-                      metrics=['accuracy'])
+model = tf.keras.Model(inputs, output_layer)
+# compile the model, set loss function, optimizer and evaluation metrics
+model.compile(
+    loss='binary_crossentropy',
+    optimizer='adam',
+    metrics=['accuracy'])
 
-deep_fm_model.fit(train_dataset, epochs=5)
+# train the model
+model.fit(train_dataset, epochs=5)
 
-test_loss, test_accuracy = deep_fm_model.evaluate(test_dataset)
-
+# evaluate the model
+test_loss, test_accuracy = model.evaluate(test_dataset)
 print('\n\nTest Loss {}, Test Accuracy {}'.format(test_loss, test_accuracy))
 
-predictions = deep_fm_model.predict(test_dataset)
-
-for prediction, rating in zip(predictions[:12], list(test_dataset)[0][1][:12]):
+# print some predict results
+predictions = model.predict(test_dataset)
+for prediction, goodRating in zip(predictions[:12], list(test_dataset)[0][1][:12]):
     print("Predicted good rating: {:.2%}".format(prediction[0]),
           " | Actual rating label: ",
-          ("Good Rating" if bool(rating) else "Bad Rating"))
-
-# tf.saved_model.save(deep_fm_model, '/Users/zhewang/Workspace/SparrowRecSys/src/main/resources/webroot/modeldata/MLPRec/005')
+          ("Good Rating" if bool(goodRating) else "Bad Rating"))
