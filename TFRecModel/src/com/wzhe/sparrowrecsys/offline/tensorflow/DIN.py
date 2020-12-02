a4dd67edf3432d63f657aff16596c18117ae866a
Conor <gekfreeman@gmail.com>
2020-12-02 18:46:09 +0800

    DIN

---
+++ TFRecModel/src/com/wzhe/sparrowrecsys/offline/tensorflow/DIN.py
@@ -0,0 +1,165 @@
+import tensorflow as tf
+from keras import backend as K
+
+# Training samples path, change to your local path
+# TRAIN_DATA_URL = "file:///Users/zhewang/Workspace/SparrowRecSys/src/main/resources/webroot/sampledata/modelSamples.csv"
+TRAIN_DATA_URL = "file:///C:/Users/Conor/Desktop/workspace/SparrowRecSys/src/main/resources/webroot/sampledata/modelSamples.csv"
+samples_file_path = tf.keras.utils.get_file("modelSamples.csv", TRAIN_DATA_URL)
+
+# load sample as tf dataset
+def get_dataset(file_path):
+    dataset = tf.data.experimental.make_csv_dataset(
+        file_path,
+        batch_size=12,
+        label_name='label',
+        na_value="0",
+        num_epochs=1,
+        ignore_errors=True)
+    return dataset
+
+# sample dataset size 110830/12(batch_size) = 9235
+raw_samples_data = get_dataset(samples_file_path)
+print(raw_samples_data)
+
+# split as test dataset and training dataset
+test_dataset = raw_samples_data.take(1000)
+train_dataset = raw_samples_data.skip(1000)
+
+# Config
+RECENT_MOVIES = 5  # userRatedMovie{1-5}
+EMBEDDING_SIZE = 10
+
+# define input for keras model
+inputs = {
+    'movieAvgRating': tf.keras.layers.Input(name='movieAvgRating', shape=(), dtype='float32'),
+    'movieRatingStddev': tf.keras.layers.Input(name='movieRatingStddev', shape=(), dtype='float32'),
+    'movieRatingCount': tf.keras.layers.Input(name='movieRatingCount', shape=(), dtype='int32'),
+    'userAvgRating': tf.keras.layers.Input(name='userAvgRating', shape=(), dtype='float32'),
+    'userRatingStddev': tf.keras.layers.Input(name='userRatingStddev', shape=(), dtype='float32'),
+    'userRatingCount': tf.keras.layers.Input(name='userRatingCount', shape=(), dtype='int32'),
+    'releaseYear': tf.keras.layers.Input(name='releaseYear', shape=(), dtype='int32'),
+
+    'movieId': tf.keras.layers.Input(name='movieId', shape=(), dtype='int32'),
+    'userId': tf.keras.layers.Input(name='userId', shape=(), dtype='int32'),
+    'userRatedMovie1': tf.keras.layers.Input(name='userRatedMovie1', shape=(), dtype='int32'),
+    'userRatedMovie2': tf.keras.layers.Input(name='userRatedMovie2', shape=(), dtype='int32'),
+    'userRatedMovie3': tf.keras.layers.Input(name='userRatedMovie3', shape=(), dtype='int32'),
+    'userRatedMovie4': tf.keras.layers.Input(name='userRatedMovie4', shape=(), dtype='int32'),
+    'userRatedMovie5': tf.keras.layers.Input(name='userRatedMovie5', shape=(), dtype='int32'),
+
+    'userGenre1': tf.keras.layers.Input(name='userGenre1', shape=(), dtype='string'),
+    'userGenre2': tf.keras.layers.Input(name='userGenre2', shape=(), dtype='string'),
+    'userGenre3': tf.keras.layers.Input(name='userGenre3', shape=(), dtype='string'),
+    'userGenre4': tf.keras.layers.Input(name='userGenre4', shape=(), dtype='string'),
+    'userGenre5': tf.keras.layers.Input(name='userGenre5', shape=(), dtype='string'),
+    'movieGenre1': tf.keras.layers.Input(name='movieGenre1', shape=(), dtype='string'),
+    'movieGenre2': tf.keras.layers.Input(name='movieGenre2', shape=(), dtype='string'),
+    'movieGenre3': tf.keras.layers.Input(name='movieGenre3', shape=(), dtype='string'),
+}
+
+# movie id embedding feature
+movie_col = tf.feature_column.categorical_column_with_identity(key='movieId', num_buckets=1001)
+movie_emb_col = tf.feature_column.embedding_column(movie_col, EMBEDDING_SIZE)
+
+# user id embedding feature
+user_col = tf.feature_column.categorical_column_with_identity(key='userId', num_buckets=30001)
+user_emb_col = tf.feature_column.embedding_column(user_col, EMBEDDING_SIZE)
+
+# genre features vocabulary
+genre_vocab = ['Film-Noir', 'Action', 'Adventure', 'Horror', 'Romance', 'War', 'Comedy', 'Western', 'Documentary',
+               'Sci-Fi', 'Drama', 'Thriller',
+               'Crime', 'Fantasy', 'Animation', 'IMAX', 'Mystery', 'Children', 'Musical']
+# user genre embedding feature
+user_genre_col = tf.feature_column.categorical_column_with_vocabulary_list(key="userGenre1",
+                                                                           vocabulary_list=genre_vocab)
+user_genre_emb_col = tf.feature_column.embedding_column(user_genre_col, EMBEDDING_SIZE)
+# item genre embedding feature
+item_genre_col = tf.feature_column.categorical_column_with_vocabulary_list(key="movieGenre1",
+                                                                           vocabulary_list=genre_vocab)
+item_genre_emb_col = tf.feature_column.embedding_column(item_genre_col, EMBEDDING_SIZE)
+
+# user behaviors
+recent_rate_col = [
+    tf.feature_column.numeric_column(key='userRatedMovie1', default_value=0),
+    tf.feature_column.numeric_column(key='userRatedMovie2', default_value=0),
+    tf.feature_column.numeric_column(key='userRatedMovie3', default_value=0),
+    tf.feature_column.numeric_column(key='userRatedMovie4', default_value=0),
+    tf.feature_column.numeric_column(key='userRatedMovie5', default_value=0),
+]
+
+# user profile
+user_profile = [
+    user_emb_col,
+    user_genre_emb_col,
+    tf.feature_column.numeric_column('userRatingCount'),
+    tf.feature_column.numeric_column('userAvgRating'),
+    tf.feature_column.numeric_column('userRatingStddev'),
+]
+
+# context features
+context_features = [
+    item_genre_emb_col,
+    tf.feature_column.numeric_column('releaseYear'),
+    tf.feature_column.numeric_column('movieRatingCount'),
+    tf.feature_column.numeric_column('movieAvgRating'),
+    tf.feature_column.numeric_column('movieRatingStddev'),
+]
+
+candidate_emb_layer = tf.keras.layers.DenseFeatures([movie_emb_col])(inputs)
+user_behaviors_layer = tf.keras.layers.DenseFeatures(recent_rate_col)(inputs)
+user_profile_layer = tf.keras.layers.DenseFeatures(user_profile)(inputs)
+context_features_layer = tf.keras.layers.DenseFeatures(context_features)(inputs)
+
+# Activation Unit
+user_behaviors_emb_layer = tf.keras.layers.Embedding(input_dim=1001,
+                                                     output_dim=EMBEDDING_SIZE,
+                                                     mask_zero=True)(user_behaviors_layer)  # mask zero
+repeated_candidate_emb_layer = tf.keras.layers.RepeatVector(RECENT_MOVIES)(candidate_emb_layer)
+
+activation_sub_layer = tf.keras.layers.Subtract()([user_behaviors_emb_layer,
+                                                   repeated_candidate_emb_layer])  # element-wise sub
+activation_product_layer = tf.keras.layers.Multiply()([user_behaviors_emb_layer,
+                                                       repeated_candidate_emb_layer])  # element-wise product
+
+activation_all = tf.keras.layers.concatenate([activation_sub_layer, user_behaviors_emb_layer,
+                                              repeated_candidate_emb_layer, activation_product_layer], axis=-1)
+
+activation_unit = tf.keras.layers.Dense(32)(activation_all)
+activation_unit = tf.keras.layers.PReLU()(activation_unit)
+activation_unit = tf.keras.layers.Dense(1, activation='sigmoid')(activation_unit)
+activation_unit = tf.keras.layers.Flatten()(activation_unit)
+activation_unit = tf.keras.layers.RepeatVector(EMBEDDING_SIZE)(activation_unit)
+activation_unit = tf.keras.layers.Permute((2, 1))(activation_unit)
+activation_unit = tf.keras.layers.Multiply()([user_behaviors_emb_layer, activation_unit])
+
+# sum pooling
+user_behaviors_pooled_layers = tf.keras.layers.Lambda(lambda x: K.sum(x, axis=1))(activation_unit)
+
+# fc layer
+concat_layer = tf.keras.layers.concatenate([user_profile_layer, user_behaviors_pooled_layers,
+                                            candidate_emb_layer, context_features_layer])
+output_layer = tf.keras.layers.Dense(128)(concat_layer)
+output_layer = tf.keras.layers.PReLU()(output_layer)
+output_layer = tf.keras.layers.Dense(64)(output_layer)
+output_layer = tf.keras.layers.PReLU()(output_layer)
+output_layer = tf.keras.layers.Dense(1, activation='sigmoid')(output_layer)
+
+model = tf.keras.Model(inputs, output_layer)
+# compile the model, set loss function, optimizer and evaluation metrics
+model.compile(
+    loss='binary_crossentropy',
+    optimizer="adam",
+    metrics=['accuracy'])
+
+# train the model
+model.fit(train_dataset, epochs=5)
+# evaluate the model
+test_loss, test_accuracy = model.evaluate(test_dataset)
+print('\n\nTest Loss {}, Test Accuracy {}'.format(test_loss, test_accuracy))
+
+# print some predict results
+predictions = model.predict(test_dataset)
+for prediction, goodRating in zip(predictions[:12], list(test_dataset)[0][1][:12]):
+    print("Predicted good rating: {:.2%}".format(prediction[0]),
+          " | Actual rating label: ",
+          ("Good Rating" if bool(goodRating) else "Bad Rating"))
