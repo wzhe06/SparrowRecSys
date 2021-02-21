f3048117301f6f710648e398e20b6815a814d289
leon <riveralex.email@gmail.com>
2021-02-21 12:51:50 -0800

    add venv and change to OneHotEncoder(PySpark 3.0)

--- RecPySpark/src/com/sparrowrecsys/offline/pyspark/featureeng/FeatureEngineering.py
+++ RecPySpark/src/com/sparrowrecsys/offline/pyspark/featureeng/FeatureEngineering.py
@@ -1,6 +1,6 @@
 from pyspark import SparkConf
 from pyspark.ml import Pipeline
-from pyspark.ml.feature import OneHotEncoderEstimator, StringIndexer, QuantileDiscretizer, MinMaxScaler
+from pyspark.ml.feature import OneHotEncoder, StringIndexer, QuantileDiscretizer, MinMaxScaler
 from pyspark.ml.linalg import VectorUDT, Vectors
 from pyspark.sql import SparkSession
 from pyspark.sql.functions import *
@@ -10,7 +10,7 @@ from pyspark.sql import functions as F
 
 def oneHotEncoderExample(movieSamples):
     samplesWithIdNumber = movieSamples.withColumn("movieIdNumber", F.col("movieId").cast(IntegerType()))
-    encoder = OneHotEncoderEstimator(inputCols=["movieIdNumber"], outputCols=['movieIdVector'], dropLast=False)
+    encoder = OneHotEncoder(inputCols=["movieIdNumber"], outputCols=['movieIdVector'], dropLast=False)
     oneHotEncoderSamples = encoder.fit(samplesWithIdNumber).transform(samplesWithIdNumber)
     oneHotEncoderSamples.printSchema()
     oneHotEncoderSamples.show(10)
@@ -20,6 +20,10 @@ def array2vec(genreIndexes, indexSize):
     genreIndexes.sort()
     fill_list = [1.0 for _ in range(len(genreIndexes))]
     return Vectors.sparse(indexSize, genreIndexes, fill_list)
+
+
+# def explode(param):
+#     pass
 
 
 def multiHotEncoderExample(movieSamples):
@@ -60,7 +64,7 @@ def ratingFeatures(ratingSamples):
 if __name__ == '__main__':
     conf = SparkConf().setAppName('featureEngineering').setMaster('local')
     spark = SparkSession.builder.config(conf=conf).getOrCreate()
-    file_path = 'file:///Users/zhewang/Workspace/SparrowRecSys/src/main/resources'
+    file_path = '/home/leon/Documents/SparrowRecSys/src/main/resources'
     movieResourcesPath = file_path + "/webroot/sampledata/movies.csv"
     movieSamples = spark.read.format('csv').option('header', 'true').load(movieResourcesPath)
     print("Raw Movie Samples:")
